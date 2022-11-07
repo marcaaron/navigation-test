ff59fb3f234add0a6ccd454a2da57e6cbf375429
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-11-07 12:46:15 -1000

    Remove transparent background

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer, DefaultTheme, CommonActions, getStateFromPath} from '@react-navigation/native';
-import {Text, View, Image, Pressable, Linking} from 'react-native';
+import {Text, View, Image, Pressable, Linking, Platform} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Constants from 'expo-constants';
-import search from './search.png';
-import avatarThree from './avatar_3.png';
-import avatarFour from './avatar_4.png';
-import avatarFive from './avatar_5.png';
-import chevron from './chevron.png';
-import chevronRight from './chevron-right.png';
 
 const chevronStyle = {width: 30, height: 30, resizeMode: 'contain', marginRight: 10};
 const chatTitleStyle = {fontSize: 18, fontWeight: 'bold'};
@@ -40,11 +34,11 @@ function LeftHandNav({navigation}) {
     <View style={{flex: 1}}>
       <LHNHeader navigation={navigation} />
       <Pressable style={{flexDirection: 'row', margin: 10, alignItems: 'center'}} onPress={() => navigation.push('Chat', {id: 1})}>
-        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: avatarFour}} />
+        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/avatar_4.png'}} />
         <Text style={chatTitleStyle}>Chat One</Text>
       </Pressable>
       <Pressable style={{flexDirection: 'row', margin: 10, alignItems: 'center'}} onPress={() => navigation.push('Chat', {id: 2})}>
-        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: avatarFive}} />
+        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/avatar_5.png'}} />
         <Text style={chatTitleStyle}>Chat Two</Text>
       </Pressable>
     </View>
@@ -53,10 +47,10 @@ function LeftHandNav({navigation}) {
 
 function AboutScreen({navigation}) {
   return (
-    <View>
+    <View style={{margin: 10}}>
       <View style={{marginBottom: 20, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
         <Pressable onPress={() => navigation.pop()}>
-          <Image style={chevronStyle} source={{uri: chevron}} />
+          <Image style={chevronStyle} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/chevron.png'}} />
         </Pressable>
         <Text style={titleStyle}>About</Text>
       </View>
@@ -76,10 +70,10 @@ function LHNHeader(props) {
       <Text style={{fontSize: 24, fontWeight: 'bold', marginLeft: 10}}>Chats</Text>
       <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', marginRight: 10, marginTop: 10}}>
         <Pressable style={{marginRight: 20}} onPress={() => props.navigation.push('Search')}>
-          <Image style={{width: 30, height: 30}} source={{uri: search}} />
+          <Image style={{width: 30, height: 30}} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/search.png'}} />
         </Pressable>
         <Pressable onPress={() => props.navigation.push('SettingsStack')}>
-          <Image style={{width: 45, height: 45, borderRadius: '50%'}} source={{uri: avatarThree}} />
+          <Image style={{width: 45, height: 45, borderRadius: '50%'}} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/avatar_3.png'}} />
         </Pressable>
       </View>
     </View>
@@ -88,20 +82,20 @@ function LHNHeader(props) {
 
 function SearchScreen({navigation}) {
   return (
-    <View>
+    <View style={{margin: 10}}>
       <View style={{marginBottom: 20, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
         <Pressable onPress={() => navigation.goBack()}>
-          <Image style={chevronStyle} source={{uri: chevron}} />
+          <Image style={chevronStyle} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/chevron.png'}} />
         </Pressable>
         <Text style={titleStyle}>Search</Text>
       </View>
       <Text style={{marginBottom: 20}}>Search Results: </Text>
       <Pressable style={{flexDirection: 'row', margin: 10, alignItems: 'center'}} onPress={() => navigation.push('Chat', {id: 1})}>
-        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: avatarFour}} />
+        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/avatar_4.png'}} />
         <Text style={chatTitleStyle}>Chat One</Text>
       </Pressable>
       <Pressable style={{flexDirection: 'row', margin: 10, alignItems: 'center'}} onPress={() => navigation.push('Chat', {id: 2})}>
-        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: avatarFive}} />
+        <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/avatar_5.png'}} />
         <Text style={chatTitleStyle}>Chat Two</Text>
       </Pressable>
     </View>
@@ -110,16 +104,16 @@ function SearchScreen({navigation}) {
 
 function SettingsScreen({navigation}) {
   return (
-    <View>
+    <View style={{margin: 10}}>
       <View style={{marginBottom: 20, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
         <Pressable onPress={() => navigation.goBack()}>
-          <Image style={chevronStyle} source={{uri: chevron}} />
+          <Image style={chevronStyle} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/chevron.png'}} />
         </Pressable>
         <Text style={titleStyle}>Settings</Text>
       </View>
       <Pressable style={{flexDirection: 'row', marginTop: 10}} onPress={() => navigation.push('About')}>
         <Text style={{fontSize: 24, flex: 1, paddingLeft: 20}}>About</Text>
-        <Image style={chevronStyle} source={{uri: chevronRight}} />
+        <Image style={chevronStyle} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/chevron-right.png'}} />
       </Pressable>
     </View>
   );
@@ -127,7 +121,7 @@ function SettingsScreen({navigation}) {
 
 function ChatScreen({route, navigation}) {
   return (
-    <View>
+    <View style={{margin: 10}}>
         <View style={{marginBottom: 20, alignItems: 'center', flexDirection: 'row', justifyContent: 'flex-start'}}>
           <Pressable onPress={() => {
               if (navigation.canGoBack()) {
@@ -137,9 +131,9 @@ function ChatScreen({route, navigation}) {
 
               console.error('On the chat screen but nowhere to go back to. We should always at least have the LHN to go back to');
           }}>
-            <Image style={chevronStyle} source={{uri: chevron}} />
+            <Image style={chevronStyle} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/chevron.png'}} />
           </Pressable>
-          <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: avatarFour}} />
+          <Image style={{width: 45, height: 45, borderRadius: '50%', marginRight: 10}} source={{uri: 'https://raw.githubusercontent.com/marcaaron/navigation-test/main/avatar_4.png'}} />
           <Text style={{fontSize: 24}}>Chat with Person {route.params.id}</Text>
         </View>
         <Pressable
@@ -192,7 +186,6 @@ const navTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: 'transparent',
   },
 };
 
@@ -206,6 +199,11 @@ export default class App extends React.Component {
 
   componentDidMount() {
     Linking.getInitialURL().then((url) => {
+      if (Platform.OS !== 'web') {
+        this.setState({initialState: getStateFromPath('', config)});
+        return;
+      }
+
       // From here we'll have a url and we need to build the initial state for the app
       // This is where things get tricky because if we have a report route then we need to parse the reportID and also make sure a LHN is the root view
       const pathname = new URL(url).pathname;
