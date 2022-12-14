35153a394197c6001e485bb27ef9155609ca83da
Vivek Kumar <vivek@expensify.com>
2022-12-14 14:21:38 +0530

    moved RHPContainer to components folder and removed the styling though screen options

---
+++ components/RHPContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Pressable, View, Dimensions} from 'react-native';
+const isSmallScreenWidth = Dimensions.get('window').width <= 800;
+
+const RHPContainer = ({navigation, children, contentStyle}) => {
+    return (
+        <View style={{flexDirection: 'row', width: '100%', height: '100%'}}>
+            {!isSmallScreenWidth && <Pressable style={{ flex: 1 }} onPress={() => navigation.goBack()}>
+                <View style={{backgroundColor: 'rgba(0, 0, 0, 0.4)', flex: 1, width: '100%', height: '100%'}} />
+            </Pressable>
+            }
+            <View style={{...contentStyle}}>
+            {
+                children
+            }
+            </View>
+        </View>
+    )
+}
+
+export default RHPContainer;
